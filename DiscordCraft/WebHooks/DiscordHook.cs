cb05a281317762ffca97f1c93dfe1dc3dad14291
William Haskell <will09600@gmail.com>
2018-05-27 03:36:15 +0200

    Allow webhook url to be accessed

--- DiscordCraft/WebHooks/DiscordHook.cs
+++ DiscordCraft/WebHooks/DiscordHook.cs
@@ -13,7 +13,7 @@ namespace DiscordCraft.WebHooks
 {
     internal static class DiscordHook
     {
-        private const string _webhookUrl =
+        public const string _webhookUrl =
             "https://discordapp.com/api/webhooks/448490925472219146/crOKy93rJkoEJeUGai5HJ_wHnCSZJ6IYTooVnbVuVbl3U0Sd0hPFeuCp4KCoCG8iONsE";
 
         public const string _avatarUrl =
