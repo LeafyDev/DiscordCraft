2735ab33301389db93b093626b9f8af6f25e67f4
William Haskell <will09600@gmail.com>
2018-05-27 17:32:30 +0200

    Add achievement type

---
+++ DiscordCraft/HookTypes/Achievement.cs
@@ -0,0 +1,24 @@
+﻿// ---------------------------------------------------------
+// Copyrights (c) 2014-2018 LeafyDev 🍂 All rights reserved.
+// ---------------------------------------------------------
+
+using System.Threading.Tasks;
+
+using Discord;
+using Discord.WebSocket;
+
+using DiscordCraft.Helpers;
+
+namespace DiscordCraft.HookTypes
+{
+    internal static class Achievement
+    {
+        public static async Task Send(SocketUserMessage msg)
+        {
+            var ach_name = msg.Content.Remove(0, 14).Split('^')[0];
+            var ach_desc = msg.Content.Split('^')[1];
+            await WebHook.SendEmbedHook("Achievement Unlocked", msg.Author.GetAvatarUrl(), msg.Author.Username,
+                16776960, $"{Format.Bold(ach_name)}\n{Format.Italics(ach_desc)}");
+        }
+    }
+}
