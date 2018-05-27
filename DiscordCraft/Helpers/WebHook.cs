7fa0d341345f68e002e6570461d5433770f96cdd
William Haskell <will09600@gmail.com>
2018-05-27 17:24:49 +0200

    Moved webhook sending to single file

---
+++ DiscordCraft/Helpers/WebHook.cs
@@ -0,0 +1,56 @@
+﻿// ---------------------------------------------------------
+// Copyrights (c) 2014-2018 LeafyDev 🍂 All rights reserved.
+// ---------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using DiscordWebhook;
+
+namespace DiscordCraft.Helpers
+{
+    internal static class WebHook
+    {
+        public static async Task SendHook(string content)
+        {
+            await new Webhook(Config.DiscordHookUrl)
+            {
+                AvatarUrl = Config.AvatarUrl,
+                Username = "SkyFactory",
+                Content = content
+            }.Send();
+        }
+
+        public static async Task SendHook(string AvatarUrl, string Username, string Content)
+        {
+            await new Webhook(Config.DiscordHookUrl)
+            {
+                AvatarUrl = AvatarUrl,
+                Username = Username,
+                Content = Content
+            }.Send();
+        }
+
+        public static async Task SendEmbedHook(string AuthorIconUrl, string AuthorName, int color, string description)
+        {
+            await new Webhook(Config.DiscordHookUrl)
+            {
+                AvatarUrl = Config.AvatarUrl,
+                Username = "SkyFactory",
+                Embeds = new List<Embed>
+                {
+                    new Embed
+                    {
+                        Author = new EmbedAuthor
+                        {
+                            IconUrl = AuthorIconUrl,
+                            Name = AuthorName
+                        },
+                        Color = color,
+                        Description = description
+                    }
+                }
+            }.Send();
+        }
+    }
+}
